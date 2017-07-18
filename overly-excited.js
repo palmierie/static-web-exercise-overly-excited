c08d30247c84575a0857486c825660933c0273a5
palmierie <ericmpalmieri@gmail.com>
2017-07-18 09:20:02 -0500

    got the exclamation mark added

--- overly-excited.js
+++ overly-excited.js
@@ -5,15 +5,20 @@ var sentence = ["The ", "walrus ", "danced ", "through ", "the ", "trees ", "in 
 
 // The addExcitement function should accept the array as the sole argument
 function addExcitement (theWordArray) {
-    /*
-      Write a `for` loop that iterates over the array argument and outputs the words
-     */
-    var newSentence = [];
-    
-    for (var i = 0; i < theWordArray.length; i++) {
-     newSentence += theWordArray[i];
-     console.log(newSentence);
-    }
+    // Write a `for` loop that iterates over the array argument and outputs the words
+  var newSentence = [];
+
+  for (var i = 0; i < theWordArray.length; i++) {
+    newSentence += theWordArray[i];
+     // console.log(newSentence);
+
+  /* Add an ! after every third word */
+    var n = i + 1
+    if(n % 3 === 0){
+    newSentence += "!";  
+    } 
+      console.log(newSentence);
+  }
 }
 
 // Invoke the function and pass in the array
